3387f99ea1110fa680c58724727eab2f8bb0b543
Andres Laguilavo <lagui2003@gmail.com>
2023-02-02 12:56:15 -0500

    animacion skills

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,23 +1,24 @@
-import React from "react";
-import "../../styles/Skills.css";
-import { generateImg } from "../../utils/GenerateImg";
+import React from "react"
+import "../../styles/Skills.css"
+import { generateImg } from "../../utils/GenerateImg"
 
 export function Skills() {
-  const skills = ["html", "css", "javascript", "react", "git"];
-
+  const skills = ["html", "css", "javascript", "react", "git"]
 
   return (
     <>
       <section className="skills-container">
         <h4 className="title-section">Skills</h4>
+
         <ul className="skills-list">
           {skills.map((skill) => (
             <li className="skills-list--item" key={skill}>
               <img src={generateImg(skill)} alt={skill} />
+              <p>{`${skill[0].toUpperCase()}${skill.substring(1)}`}</p>
             </li>
           ))}
         </ul>
       </section>
     </>
-  );
+  )
 }
